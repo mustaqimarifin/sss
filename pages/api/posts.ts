a41d22184fc364852c0b100df75c80292bebd739
vmp <xyz@xyz.local>
2023-01-20 16:43:35 +0800

    sizze down

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -9,17 +9,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const key: RedisKey = 'posts';
-  const posts = await sanityClient.fetch(indexQuery);
+  //const key: RedisKey = 'posts';
+  const posts: PostPageGroup = await sanityClient.fetch(indexQuery);
 
-  const postcache = await cache.fetch(key, () => posts, 60 * 60 * 24);
+  /*   const postcache = await cache.fetch(key, () => posts, 60 * 60 * 24);
 
   console.log(postcache);
   if (postcache) {
     const getcache: PostPageGroup = await cache.get(key);
     console.log(key);
     return res.status(200).json(getcache);
-  } else {
-    return res.status(200).json(posts);
-  }
+  } else { */
+  return res.status(200).json(posts);
 }
